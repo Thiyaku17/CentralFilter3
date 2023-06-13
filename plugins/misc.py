0b4fc62b53249641b90322aa020b43fe3aa43c54
CentralLinks17 <109009847+CentralLinks17@users.noreply.github.com>
2023-06-13 15:30:34 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -145,6 +145,9 @@ async def imdb_search(client, message):
             for movie in movies
         ]
         await k.edit('Here is what i found on IMDb', reply_markup=InlineKeyboardMarkup(btn))
+        await asyncio.sleep(600)
+        await message.delete()
+        await k.delete()        
     else:
         await message.reply('Give me a movie / series Name')
 
